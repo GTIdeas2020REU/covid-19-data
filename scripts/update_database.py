71753bc75067ee707191d5ba67b52232e969f8e1
Aniruddha <aniruddha.murali@gmail.com>
2020-07-30 23:19:32 -0400

    Rewrote script to insert point forecasts into database

--- scripts/update_database.py
+++ scripts/update_database.py
@@ -8,8 +8,8 @@ sys.path.append("../")
 
 client = pymongo.MongoClient(getClient())
 mydb = client['covid19-forecast']
-mycol = mydb['forecasts']
-files = glob.glob("../forecasts_processed/*.csv")
+mycol = mydb['point_forecasts']
+files = glob.glob("../forecasts_processed/point/*.csv")
 
 models_list = []
 for file in files:
@@ -21,28 +21,37 @@ for file in files:
     filetype = file.find('.csv')
     model_name = file[last_slash + 1 : filetype]
 
-    df.astype({'quantile': 'float', 'value': 'float'}).dtypes
+    df = df.drop_duplicates(subset=['target_end_date', 'location'], keep='last')
+    df = df.loc[(df['target'].str.contains("wk"))]
+
+    #df.astype({'quantile': 'float', 'value': 'float'}).dtypes
+    df.astype({'value': 'float'}).dtypes
 
     locations = list(df['location'].array)
     locations = [int(loc) for loc in locations if loc != 'US']
     targets = list(df['target'].array)
-    types = list(df['type'].array)
-    quantiles = list(df['quantile'].array)
-    quantiles = [float(quantile) for quantile in quantiles]
-    forecast_date = df['forecast_date'].array[0]
+    #types = list(df['type'].array)
+    #quantiles = list(df['quantile'].array)
+    #quantiles = [float(quantile) for quantile in quantiles]
+    #forecast_date = df['forecast_date'].array[0]
+    forecast_date = list(df['forecast_date'].array)
     target_dates = list(df['target_end_date'].array)
     values = list(df['value'].array)
     values = [float(value) for value in values]
+    print(model_name)
 
+    # For now only inserting documents with point forecasts
     model_dict = {'model': model_name, 
                   'location': locations, 
                   'target': targets,
-                  'type': types,
-                  'quantile': quantiles,
+                  #'type': types,
+                  #'quantile': quantiles,
                   'forecast_date': forecast_date,
                   'target_end_date': target_dates,
                   'value': values}
 
-    models_list.append(model_dict)
+    x = mycol.insert_one(model_dict)
 
-x = mycol.insert_many(models_list)
+    #models_list.append(model_dict)
+
+#x = mycol.insert_many(models_list)
